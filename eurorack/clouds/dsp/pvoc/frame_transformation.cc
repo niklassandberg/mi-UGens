076334896c25fafaee543c1b163f6c6be52e9e30
Niklas Sandberg <niklas1sandberg@gmail.com>
2026-03-26 10:57:12 +0100

    Same as before, forgot just testing.

--- eurorack/clouds/dsp/pvoc/frame_transformation.cc
+++ eurorack/clouds/dsp/pvoc/frame_transformation.cc
@@ -99,7 +99,6 @@ void FrameTransformation::Process(
 
   }
 
-  float* temp = &fft_out[0];
 
   ReplayMagnitudes(ifft_in, parameters.position);
 
@@ -114,7 +113,13 @@ void FrameTransformation::Process(
   BlendFeedback(fft_working_frames_[1], 0.0f, parameters.spectral.refresh_rate, fft_working_frames_, 7);
   copy(fft_working_frames_[0], fft_working_frames_[0] + size_, ifft_in);
 */
-  BlendFeedback(ifft_in, 0.0f, parameters.spectral.refresh_rate, fft_working_frames_, 7);
+
+  copy(ifft_in, ifft_in + size_, fft_working_frames_[0]);
+  BlendFeedback(ifft_in, 1.0f, parameters.spectral.refresh_rate, fft_working_frames_, 7);
+  copy(fft_working_frames_[0], fft_working_frames_[0] + size_, ifft_in);
+
+  float* temp = &ifft_in[0];
+
 
   WarpMagnitudes(ifft_in, temp, parameters.spectral.warp);
   ShiftMagnitudes(temp, ifft_in, pitch_ratio);
