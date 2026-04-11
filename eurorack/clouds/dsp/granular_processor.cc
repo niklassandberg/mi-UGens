ee64454e45e3092acb4a85df46d561c9edce36eb
Niklas Sandberg <niklas1sandberg@gmail.com>
2026-04-11 03:07:27 +0200

    Moving back to FillSineWindow because of testing.

--- eurorack/clouds/dsp/granular_processor.cc
+++ eurorack/clouds/dsp/granular_processor.cc
@@ -471,7 +471,7 @@ void GranularProcessor::Prepare() {
       static float window[kMaxFftSize];
       static bool window_ready = false;
       if (!window_ready) {
-        FillHannWindow(window, kMaxFftSize);  // or FillSineWindow
+        FillSineWindow(window, kMaxFftSize);  // or FillSineWindow/FillHannWindow
         window_ready = true;
       }
       phase_vocoder_.Init(
